24bdf191bcf08c2e5d6474deaf0a6cf1e8043f03
Vakarchuk.DM <ozk.nemoy@mail.ru>
2018-11-02 15:08:36 +0300

    start impl parser

--- src/parse-xls.ts
+++ src/parse-xls.ts
@@ -2,28 +2,33 @@
 import * as fs from "fs";
 import xlsx from 'node-xlsx';
 import {HandleData} from '../../staffjs/src/client/app/shared/services/handle-data';
-import {INodeXlsxParsed} from "../../staffjs/src/server/interfaces/node-xlsx";
-import {IPersonnel, IPersonnelAdapter} from "../../staffjs/src/server/components/personnel/personnel.interface";
+import {IPersonnel} from "../../staffjs/src/server/components/personnel/personnel.interface";
 
 
 
 export class ParseXls {
 
-  constructor() {
-
+  static createFromFile(personnelId, file) {
+    const w = xlsx.parse(file)[0];
+    return  this.parse(personnelId, w.data[1]);
   }
 
   static create(personnelId, excelPath = './src/staff.xls') {
     const w = xlsx.parse(fs.readFileSync(excelPath))[0];
-    return  ParseXls.parse(personnelId, w.data[1]);
+    return  this.parse(personnelId, w.data[1]);
   }
 
   static splitByN(_str, n, splitter = ' ') {
     const strArr = _str.split(splitter);
     let out = [];
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < strArr.length; i++) {
       if(strArr[i]) {
-        out.push(strArr[i])
+        // пишем лишнее в последний элемент разбивая с помощью splitter
+        if (i > n - 1) {
+          out[n - 1] = out[n - 1] + splitter + strArr[i]
+        } else {
+          out.push(strArr[i])
+        }
       } else {
         out.push('')
       }
@@ -32,49 +37,45 @@ export class ParseXls {
   }
 
   static parse(personnelId: number, xls: string[]) {
-    const [name, surname, middleName] = ParseXls.splitByN(xls[1], 3);
+    const [name, surname, middleName] = this.splitByN(xls[1], 3);
 
     const worker: Partial<IPersonnel> = {
       number: xls[0],
       surname,
       name,
       middleName,
-      inn: xls[4],
-      insurance: xls[5],
-      passport: {
-        birthDate: xls[6],
-        birthPlace: xls[7],
-        citizenship: xls[8],
-        maritalStatus: xls[9],
-        number: xls[10],
-        passportIssued: xls[11],
-        passportDate: xls[12],
-        address: xls[13],
-        passportRegDate: xls[14],
-      },
-      educationName: xls[15],
-      institutions: /*<Partial<IPersonnel['institutions']>>*/[{
-        /*id: null,
-        docName: null,*/
-        personnelId,
-        name: xls[16],
+      inn: xls[2],
+      insurance: xls[3],
+      /*passport*/
+      educationName: xls[13],
+      /*institutions*/
+    };
+    const passport: Partial<IPersonnel['passport']> = {
+      personnelId,
+      birthDate: HandleData.ruDateToServer(xls[4]),
+      birthPlace: xls[5],
+      citizenship: xls[6],
+      maritalStatus: xls[7],
+      number: xls[8],
+      passportIssued: xls[9],
+      passportDate: HandleData.ruDateToServer(xls[10]),
+      address: xls[11],
+      passportRegDate: HandleData.ruDateToServer(xls[12]),
+    };
+    const institution: Partial<IPersonnel['institutions'][0]> = {
+      personnelId,
+      name: xls[14],
 
-        endDate: ParseXls.setYear(xls[18]),
-        qualification: xls[19],
-        specialty: xls[20],
-        docNumber: xls[21],// T
-      }]
+      endDate: HandleData.setYear(xls[16]),
+      qualification: xls[17],
+      specialty: xls[18],
+      docNumber: xls[19],// T
     };
 
 
-    return worker
-  }
-
-  static setYear(year) {
-    if(!year) return null;
-    const d = new Date();
-    d.setFullYear(year, 1, 1);
-    return HandleData.dateToServer(d.toString())
+    return {
+      worker, passport, institution
+    }
   }
 
 }
