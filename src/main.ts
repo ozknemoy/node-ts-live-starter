a59a5eda01fdf0261b1c84d5250fe121e8bf5e5b
Vakarchuk.DM <ozk.nemoy@mail.ru>
2019-01-16 15:07:50 +0300

    impl faker for personnel staffjs

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
-
-console.log('start');
-
+import {departments} from "./departments";
 const faker:Faker.FakerStatic = require('faker/locale/ru');
-
-
-//import {HandleData} from '../../staffjs/src/shared/handle-data';
+import {HandleData} from '../../staffjs/src/shared/handle-data';
 import {IPersonnel} from "../../staffjs/src/server/components/personnel/personnel.interface";
+import {specialties} from "./specialties";
+
+const depLength = departments.length;
+const categories = Object.keys(specialties);
+const catLength = categories.length;
+
+
 function past(years?: number) {
   return faker.date.past(years).toISOString()
 }
 
-function between(from: number, to: number) {
+function booleanRareTrue(n = 6): boolean {
+  return !faker.random.number({max:n})
+}
+
+function dateBetween(from: number, to: number) {
   let _from = faker.date.recent(100);
   _from.setFullYear(2018 - from);
   _from.toISOString();
@@ -19,31 +26,27 @@ function between(from: number, to: number) {
   return faker.date.between(_from, _to).toISOString();
 }
 
-console.log(between(20, 11));
 export class FakePersonnel {
   static create() {
     let ret = [];
-    for (var i = 0; i < 1; i++) {
+    for (let i = 0; i < 1; i++) {
       ret.push(FakePersonnel.build())
     }
     return ret
   }
+
   static build() {
-
     const [surname, name] = faker.name.findName().split(' ');
-
     const worker: Partial<IPersonnel> = {
-      number: faker[0],
+      number: faker.random.number({min:1000, max:9999}).toString(),
       surname,
       name,
       middleName: faker.lorem.word(),
       inn: faker.lorem.word(),
       insurance: faker.random.uuid(),
       educationName: faker.lorem.word(),
-
       workExpDate: faker.date.recent().toISOString(),
       profession: faker.name.jobType(),
-
       afterInstEduName: faker.lorem.word(),
       membershipGAN: faker.random.boolean(),
       membershipGANDate: past(5),
@@ -56,122 +59,123 @@ export class FakePersonnel {
       disabilityDegree: faker.lorem.words(),
     };
     const passport: Partial<IPersonnel['passport']> = {
-      birthDate: between(80, 20),
-      birthPlace: faker[5],
-      citizenship: faker[6],
-      maritalStatus: faker[7],
+      birthDate: dateBetween(80, 20),
+      birthPlace: `${faker.address.zipCode()}, ${faker.address.city()} ${faker.address.streetName()}  ${faker.address.streetAddress()} `,
+      citizenship: faker.address.county(),
+      maritalStatus: faker.lorem.word(),
       number: faker.random.uuid().slice(4, 15),
-      passportIssued: faker[9],
-      passportDate: between(30, 11),
+      passportIssued: dateBetween(20, 5),
+      passportDate: dateBetween(30, 11),
       address: `${faker.address.zipCode()}, ${faker.address.city()} ${faker.address.streetName()}  ${faker.address.streetAddress()} `,
-      passportRegDate: between(20, 11),
+      passportRegDate: dateBetween(20, 11),
     };
     // https://rawgit.com/Marak/faker.js/master/examples/browser/index.html#date
-    console.log(passport);
-    /*const institution: Partial<IPersonnel['institutions'][0]> = {
-      name: faker[14],
 
-      endDate: HandleData.setYear(faker[16]),
-      qualification: faker[17],
-      specialty: faker[18],
-      docNumber: faker[19],// T
+
+    const institution: Partial<IPersonnel['institutions'][0]> = {
+      name: faker.lorem.words(4),
+      endDate: dateBetween(30, 11),
+      qualification: faker.lorem.word(),
+      specialty: faker.lorem.word(),
+      docNumber: faker.random.number({min:100000, max:999999}).toString()
     };
     const scientificInst: Partial<IPersonnel['scientificInst'][0]> = {
-      name: faker[21],
-      fullInfo: faker[22],
-      endDate: HandleData.setYear(faker[23]),
-      specialty: faker[24],
-      academicDegree: faker[111],
-      scienceBranch: faker[26],
-      dateAndNumber: faker[29],
-      dissertationCouncil: faker[28],
+      name: faker.lorem.words(4),
+      fullInfo: faker.lorem.words(8),
+      endDate: faker.random.number({min:1950, max:2017}).toString(),
+      specialty: faker.lorem.words(2),
+      academicDegree: faker.lorem.word(),
+      scienceBranch: faker.lorem.words(2),
+      dateAndNumber: faker.random.number({min:100000, max:999999}).toString(),
+      dissertationCouncil: faker.lorem.words(4),
     };
     const academicRank: Partial<IPersonnel['academicRank'][0]> = {
-      rank: faker[112],
-      specialty: faker[33],
-      docNumber: faker[30],
-      docDate: HandleData.ruDateToServer(faker[31]),
-      appointingAuthority: faker[32],
+      rank: faker.lorem.words(2),
+      specialty: faker.lorem.words(2),
+      docNumber: faker.random.number({min:100000, max:999999}).toString(),
+      docDate: dateBetween(11, 3),
+      appointingAuthority: faker.lorem.words(2),
     };
+
+    const category = categories[faker.random.number({min:0, max:catLength-1})];
+    const specialty: string = faker.random.arrayElement(specialties[category]);
+
 
     const workplaces: Partial<IPersonnel['workplaces'][0]> = {
-      date: HandleData.ruDateToServer(faker[42] || faker[56]),
-      department: faker[34],
-      specialty: faker[35],
-      reason: faker[43] || faker[57],
-      academicCouncilDate: HandleData.ruDateToServer(faker[120]),
-      attractionTerms,
-      rate: +faker[37],
-      duration: +faker[108],
-      category: faker[37],
-      dismissalDate: HandleData.ruDateToServer(faker[74]),
-      dismissalGround: faker[75],
-      lawArticle: faker[77],
+      date: faker.lorem.words(2),
+      department: departments[faker.random.number({min:0, max:depLength-1})],
+      specialty,
+      reason: faker.lorem.words(2),
+      academicCouncilDate: dateBetween(11, 3),
+      attractionTerms: faker.lorem.words(2),
+      rate: faker.random.number({min:2, max:4}) * 0.25,
+      duration: faker.random.number({min:30, max:40}),
+      category,
+      salaryCoef: faker.random.number({min:10, max:26}) / 10,
+      dismissalDate: dateBetween(3, -1),
+      dismissalGround: faker.lorem.words(2),
+      lawArticle: faker.lorem.words(2),
+      active: !booleanRareTrue(10000),
+      contractNumber: faker.random.number({min:100000, max:999999}).toString(),
+      contractDate: dateBetween(-1, -2),
+      contractEndDate: dateBetween(-1, -2),
     };
-    const workExp: Partial<IPersonnel['workExp']> = this.getWorkExp(faker, null);
-    const laborContracts: Partial<IPersonnel['laborContract'][0]> = {
-      number: faker[39],
-      date: HandleData.ruDateToServer(faker[40]),
-      endDate: HandleData.ruDateToServer(faker[41]),
-      specialty: faker[35],
-      department: faker[34],
-      attractionTerms,
-    };
-    const rewards/!*: Partial<IPersonnel['rewards']>*!/ = this.getRewards(faker);
-
+    const rewards = this.getRewards();
+    const workExp: Partial<IPersonnel['workExp']> = this.getWorkExp(null);
+    console.log("*****",workplaces);
     return {
       worker, rewards,academicRank, passport, institution, scientificInst ,
-      workplaces, workExp, laborContracts
-    }*/
+      workplaces, workExp
+    }
   }
 
-  static getRewards(faker) {
+  static getRewards() {
     const r = [];
-    if(faker[62]) r.push('ОрденаМедали');
-    if(faker[64]) r.push('ВедомствНагр');
-    if(faker[65]) r.push('ПремииСПб');
-    if(faker[66]) r.push('ЗвЗаслужИгоспремии');
-    if(faker[67]) r.push('НагрГУАП');
-    if(faker[68]) r.push('ПрочиеНагр');
-    if(faker[71]) r.push('РегионНагр');
-    if(faker[93]) r.push('ЗаслДеятН');
-    if(faker[94]) r.push('ЗаслРабВШ');
-    if(faker[95]) r.push('ЗаслРабСПО');
-    if(faker[96]) r.push('ЗаслЮр');
-    if(faker[97]) r.push('ПочРабВПО');
-    if(faker[98]) r.push('РазвНИРстуд');
-    if(faker[99]) r.push('ЗаслПрофГУАП');
-    if(faker[100]) r.push('ПочРабСфМолП');
-    if(faker[101]) r.push('ПочРабОбО');
-    if(faker[110]) r.push('ПочРабНауки');
+    if(booleanRareTrue()) r.push('ОрденаМедали');
+    if(booleanRareTrue()) r.push('ВедомствНагр');
+    if(booleanRareTrue()) r.push('ПремииСПб');
+    if(booleanRareTrue()) r.push('ЗвЗаслужИгоспремии');
+    if(booleanRareTrue()) r.push('НагрГУАП');
+    if(booleanRareTrue()) r.push('ПрочиеНагр');
+    if(booleanRareTrue()) r.push('РегионНагр');
+    if(booleanRareTrue()) r.push('ЗаслДеятН');
+    if(booleanRareTrue()) r.push('ЗаслРабВШ');
+    if(booleanRareTrue()) r.push('ЗаслРабСПО');
+    if(booleanRareTrue()) r.push('ЗаслЮр');
+    if(booleanRareTrue()) r.push('ПочРабВПО');
+    if(booleanRareTrue()) r.push('РазвНИРстуд');
+    if(booleanRareTrue()) r.push('ЗаслПрофГУАП');
+    if(booleanRareTrue()) r.push('ПочРабСфМолП');
+    if(booleanRareTrue()) r.push('ПочРабОбО');
+    if(booleanRareTrue()) r.push('ПочРабНауки');
 
     return r.map((name) => ({name}))
   }
 
-  static getWorkExp(faker, personnelId): Partial<IPersonnel['workExp']> {
+  static getWorkExp(personnelId): Partial<IPersonnel['workExp']> {
     return <Partial<IPersonnel['workExp']>>[{
       id: null,
       personnelId,
       typeId: 1,
-      amountY: parseInt(faker[46]),
-      amountM: parseInt(faker[47]),
-      amountD: parseInt(faker[48]),
+      amountY: faker.random.number({min:1, max:10}),
+      amountM: faker.random.number({min:1, max:10}),
+      amountD: faker.random.number({min:1, max:30}),
     },{
       id: null,
       personnelId,
       typeId: 2,
-      amountY: parseInt(faker[49]),
-      amountM: parseInt(faker[50]),
-      amountD: parseInt(faker[51]),
+      amountY: faker.random.number({min:1, max:10}),
+      amountM: faker.random.number({min:1, max:10}),
+      amountD: faker.random.number({min:1, max:30}),
     },{
       id: null,
       personnelId,
       typeId: 3,
-      amountY: null,
-      amountM: null,
-      amountD: null,
+      amountY: faker.random.number({min:0, max:10}),
+      amountM: faker.random.number({min:0, max:10}),
+      amountD: faker.random.number({min:0, max:30}),
     }]
   }
 }
 
-console.log(FakePersonnel.create());
+FakePersonnel.create();
