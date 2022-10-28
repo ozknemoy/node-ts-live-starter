15e0571dea0f0c8ed0e822edff867a2656dc4d86
VakarchukD <ozk.nemoy@mail.ru>
2022-10-28 14:09:15 +0300

    add not-strict file to strict-lib and include it to root tsconfig

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 // not-strict-null-check
 import { NULL } from './strict-null-check-lib';
 import { ID_NULLABLE } from './not-strict-null-check-lib';
+import { ID_NULLABLE_FROM_STRICT_LIB } from './strict-null-check-lib/index-not-strict-null-check';
 
 let id: number = null;
 const nul: number = NULL;
 const idNullable: number = ID_NULLABLE;
 
-console.log(id, nul, idNullable);
+console.log(id, nul, idNullable, ID_NULLABLE_FROM_STRICT_LIB);
---
+++ src/strict-null-check-lib/index-not-strict-null-check.ts
@@ -0,0 +1,1 @@
+export const ID_NULLABLE_FROM_STRICT_LIB: number = null;
